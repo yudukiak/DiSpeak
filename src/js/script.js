35d33a6559cb92e2a8ba4f7cc1620a69ceeba6cf
micelle <micelle_yamame@hotmail.co.jp>
2019-01-15 09:49:08 +0900

    [fix] バッファの修正

--- src/js/script.js
+++ src/js/script.js
@@ -1034,8 +1034,8 @@ function bouyomiSpeak(data) {
     buffer.writeUInt16LE(0xFFFF, 2);
     buffer.writeUInt16LE(0xFFFF, 4);
     buffer.writeUInt16LE(0xFFFF, 6);
-    buffer.writeUInt16LE(0000, 8);
-    buffer.writeUInt8(00, 10);
+    buffer.writeUInt16LE(0x0000, 8);
+    buffer.writeUInt8(0x00, 10);
     buffer.writeUInt32LE(messageBuffer.length, 11);
     messageBuffer.copy(buffer, 15, 0, messageBuffer.length);
     bouyomiClient.write(buffer);
