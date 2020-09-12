93a2a31f5fb5d50186c1bcfd3f5ac58fb8e79c84
紅坂柚月 <micelle_yamame@hotmail.co.jp>
2020-09-12 18:47:47 +0900

    [update] デフォルトアイコンの調整 #80

--- src/js/script.js
+++ src/js/script.js
@@ -1083,7 +1083,10 @@ client.on('message', function(data) {
     return data.author.note;
   })();
   //const avatarURL = data.author.displayAvatarURL.replace(/\?size=\d+/, '');
-  const avatarURL = `https://cdn.discordapp.com/avatars/${data.author.id}/${data.author.avatar}.png`;
+  const avatarURL = (function(){
+    if (data.author.avatar == null) return data.author.defaultAvatarURL;
+    return `https://cdn.discordapp.com/avatars/${data.author.id}/${data.author.avatar}.png`;
+  })();
   // チャットの内容
   let content = data.content;
   // NGワードの処理
