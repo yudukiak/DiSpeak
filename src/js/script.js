c131e5408d31cb9d1c9699427e18681eb046921d
紅坂柚月 <micelle_yamame@hotmail.co.jp>
2020-09-15 12:10:40 +0900

    [fix] 設定が消える現象の対応

--- src/js/script.js
+++ src/js/script.js
@@ -875,6 +875,7 @@ client.on('ready', function() {
   debugLog('[Discord] sessionStorage', sStorage);
   if (sStorage == null) {
     readFile(); // 設定ファイルを反映
+    writeFile();
   } else {
     const sStorageObj = JSON.parse(sStorage);
     const lStorageId = localStorage.getItem('DiscordLoginId');
@@ -1604,7 +1605,7 @@ function loginDiscord(token) {
     .then(function(res) {
       client.user.setStatus(client.user.settings.status);
       //client.user.settings.update('status', client.user.settings.status);
-      writeFile();
+      //writeFile();
     }).catch(function(err) {
       M.Modal.getInstance($('#modal_discord')).close();
       const txt = String(err);
